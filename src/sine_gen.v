7b134f4c63bd91faa2c1417439ec3e9e9fc04f3b
Chshy <szclsxycs@163.com>
2023-04-18 22:40:42 +0800

    sine_wav_gen

---
+++ src/sine_gen.v
@@ -0,0 +1,31 @@
+module sine_gen #(
+    parameter ADDR_WIDTH = 12,
+    parameter DATA_WIDTH = 32,
+    parameter INIT_ADDR  = 12'h000,
+    parameter FILE_PATH  = "sine_data.mif"
+) (
+    input                         clk,
+    input                         rst_n,
+    input      [ADDR_WIDTH - 3:0] step,
+    output reg [DATA_WIDTH - 1:0] sine_wav // signed
+);
+
+
+reg [DATA_WIDTH - 1:0] sine_data[2**(ADDR_WIDTH):1]; // 补码
+reg [ADDR_WIDTH - 1:0] addr;
+
+
+initial $readmemh(FILE_PATH, sine_data);
+
+always @(posedge clk or negedge rst_n) begin
+    if (~rst_n) begin
+        addr <= INIT_ADDR;
+    end else begin
+        sine_wav <= sine_data[addr];
+        addr <= addr + step;
+    end
+
+end
+
+
+endmodule
